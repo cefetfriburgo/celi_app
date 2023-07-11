4ae3e36717dc1cd8d777463a042bcc5395265584
Joao G <joao.guylherme@hotmail.com>
2023-07-11 14:40:30 -0300

    Fixed merge

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,42 +14,40 @@
 </head>
 
 <body>
-<header>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container">
-            <a class="navbar-brand" href="index.html">
-                <img src="assets/img/logo-white-e1535748387518.png" alt="Logo"
-                     class="imageWidth d-inline-block align-text-top">
-            </a>
-            <a class="btn btn-dark" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
-               aria-controls="offcanvasExample">
-                <i class="bi bi-list fs-4"></i>
-            </a>
-        </div>
-    </nav>
-
-        <!-- MENU LATERAL -->
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample"
-            aria-labelledby="offcanvasExampleLabel">
-            <div class="offcanvas-header">
-                <p class="m-0">Menu</p>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <header>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">
+                    <img src="assets/img/logo-white-e1535748387518.png" alt="Logo" class="imageWidth d-inline-block align-text-top">
+                </a>
+                <a class="btn btn-dark" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                    <i class="bi bi-list fs-4"></i>
+                </a>
             </div>
-            <div class="offcanvas-body">
-                <div class="dropdown mt-3">
-                    <ul class="navbar-nav">
-                        <li class="nav-item"><a href="login" class="nav-link">Login</a></li>
-                        <li class="nav-item"><a href="cursos" class="nav-link">Cursos</a></li>
-                        <li class="nav-item"><a href="como-participar" class="nav-link">Como participar</a></li>
-                        <li class="nav-item"><a href="contatos" class="nav-link">Contatos</a></li>
-                        <li class="nav-item"><a href="sobre-nos" class="nav-link">Sobre nós</a></li>
-                    </ul>
-                </div>
+        </nav>
+    </header>
+
+    <!-- MENU LATERAL -->
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+        <div class="offcanvas-header">
+            <p class="m-0">Menu</p>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <div class="dropdown mt-3">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="login" class="nav-link">Login</a></li>
+                    <li class="nav-item"><a href="cursos" class="nav-link">Cursos</a></li>
+                    <li class="nav-item"><a href="como-participar" class="nav-link">Como participar</a></li>
+                    <li class="nav-item"><a href="contatos" class="nav-link">Contatos</a></li>
+                    <li class="nav-item"><a href="sobre-nos" class="nav-link">Sobre nós</a></li>
+                </ul>
             </div>
         </div>
     </div>
-</main>
-<footer>
+    </div>
+    </main>
+    <footer>
 
 </body>
 
