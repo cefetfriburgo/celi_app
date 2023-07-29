1308a7e92faa8a23f4919631b83176845a20509b
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2023-07-29 18:03:52 -0300

    Implementacao da funcao de exibir todos os eventos, criacao da migration create_eventos_table, criacao do modelo Evento

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Evento;
 use Illuminate\Http\Request;
 
 class EventoController extends Controller
 {
     public function eventos(){
-        return view('eventos');
+        $eventos = Evento::all();
+        return view('eventos', ['eventos' => $eventos]);
     }
 }
--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -6,37 +6,15 @@ Eventos
 
 @section('principal')
 <div class="row">
-        <div class="card mb-3">
-            <h5 class="card-header">Festa</h5>
-            <div class="card-body">
-              <h5 class="card-title">Festa Junina</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="festa.html" class="btn btn-primary">Veja mais</a>
-            </div>
-          </div>
-          <div class="card">
-            <h5 class="card-header">Oficina</h5>
-            <div class="card-body">
-              <h5 class="card-title">Curso bootstrap</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="festa.html" class="btn btn-primary">Veja mais</a>
-            </div>
-          </div>
-          <div class="card">
-            <h5 class="card-header">Evento esportivo</h5>
-            <div class="card-body">
-              <h5 class="card-title">Curso bootstrap</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="festa.html" class="btn btn-primary">Veja mais</a>
-            </div>
-          </div>
-          <div class="card">
-            <h5 class="card-header">Oficina</h5>
-            <div class="card-body">
-              <h5 class="card-title">Curso bootstrap</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="festa.html" class="btn btn-primary">Veja mais</a>
-            </div>
-          </div>
-        </div>
+  @foreach($eventos as $evento)
+  <div class="card m-3">
+    <h5 class="card-header">{{$evento->tipo}}</h5>
+    <div class="card-body">
+      <h5 class="card-title">{{$evento->titulo}}</h5>
+      <p class="card-text">{{$evento->descricao}}</p>
+      <a href="festa.html" class="btn btn-primary">Veja mais</a>
+    </div>
+  </div>
+@endforeach
+</div>
 @endsection
