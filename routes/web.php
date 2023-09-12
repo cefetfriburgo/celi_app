c6fc660596009e9cc8c3efdc60f9a0a06228fa7e
extensao <extensao@extensao.extensao.local>
2023-09-12 14:26:02 -0300

    Exclusão da migration session duplicada

--- routes/web.php
+++ routes/web.php
@@ -106,3 +106,12 @@ Route::delete();
  *              public function delete(id){ ... }
  *
  */
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
