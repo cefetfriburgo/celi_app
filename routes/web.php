1c20d6f0a6e61b0f3f0a90910796ba38d1145fb8
JoaoLuizRSilva <61887901+JoaoLuizRSilva@users.noreply.github.com>
2024-11-22 15:51:09 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ use Laravel\Jetstream\Rules\Role;
 |
 */
 //INDEX & VARIADAS
+/*
 Route::get('/', [IndexController::class, 'showHome'])->name('showHome');
 Route::get('/contatos', [IndexController::class,'showContatos']);
 Route::get('/comoParticipar', [IndexController::class,'showComoParticipar'])->name('showComoParticipar');
@@ -108,6 +109,7 @@ Route::delete();
  *              public function delete(id){ ... }
  *
  */
+/*
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -116,4 +118,4 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
+});*/
